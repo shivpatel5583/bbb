fb6f74f609d081241c44f09da13abc0b3def7a83
shivpatel5583 <108368317+shivpatel5583@users.noreply.github.com>
2023-12-09 10:25:40 -0500

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,1 +1,61 @@
+public class Product {
 
+    private String name;
+
+    private Store owner;
+
+    private String description;
+
+    private int quantity;
+
+    private double price;
+
+    public Product(String name, Store owner, String description, int quantity, double price) {
+        this.name = name;
+        this.owner = owner;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+        this.owner.getProductList().add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Store getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Store owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
