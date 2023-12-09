8bd4c3a816bed1894b1ef896cc7fe3e37a576de9
shivpatel5583 <108368317+shivpatel5583@users.noreply.github.com>
2023-12-09 10:28:08 -0500

    Update Store.java

--- Store.java
+++ Store.java
@@ -1,1 +1,43 @@
+import java.util.ArrayList;
+
+public class Store {
+    private String storeName;
+
+    private Seller owner;
+    private ArrayList<Product> productList;
+
+    private ArrayList<String> sales;
+
+    public Store(String storeName, Seller owner) {
+        this.storeName = storeName;
+        this.owner = owner;
+        this.owner.getStoreLists().add(this);
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public Seller getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Seller owner) {
+        this.owner = owner;
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(ArrayList<Product> products) {
+        this.productList = products;
+    }
+
+
+}
 
